bf177ee44a9e013195f92c2478910d037f0f7e78
feed-mina <myelin24@naver.com>
2022-12-16 19:28:21 +0900

    HikariDataSource 가 생성자주입으로 dataSource로 들어간다

--- demo/demo/src/main/java/com/example/demo/repository/RoleDao.java
+++ demo/demo/src/main/java/com/example/demo/repository/RoleDao.java
@@ -1,14 +1,26 @@
 package com.example.demo.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-// @ 이거 하나만 붙어줬는데 스프링에서 인스턴스를 메모리에 만들어서 관리한다는 의미
-// 스프링부트에서 demoApplication은 패키지가 package com.example.demo로 되어있다. DemoApplication은 설정파일이고 그 의미는
-// demo 이하로만 컴포넌트를 찾아서 레포지토리로 등록한다라는 의미이다.
+import javax.sql.DataSource;
 
+// Spring JDBC를 이용해서 Database 프로그래밍
+// Repository는 @Component 이고 컴포넌트는 컨테이너가 관리하는 Bean이 된다.
 @Repository
 public class RoleDao {
-    public RoleDao(){
+    // JDBC 탬플릿
+    private final JdbcTemplate jdbcTemplate; //필드를 final로 선언하면 반드시 생성자에서 초기화 한다.
+
+//    public RoleDao(org.springframework.jdbc.core.JdbcTemplate jdbcTemplate) {
+//        JdbcTemplate = jdbcTemplate;
+//    }
+
+    // 생성자에 파라미터를 넣어주면 스프링 부트가 자동으로 주입한다. -> 생성자 주입이라고한다.
+    // 생성자 주입 : 히카리CP가 만들어주는 것
+    public RoleDao(DataSource dataSource){
         System.out.println("RoleDao 생성자 호출");
+        System.out.println(dataSource.getClass().getName());
+        jdbcTemplate = new JdbcTemplate();
     }
 }
